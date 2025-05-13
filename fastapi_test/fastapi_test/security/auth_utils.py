88a540b5673245c7b103aa284df944ce7fb6b501
Andrea Bonatti <andreabonatti92@gmail.com>
2025-05-13 12:22:44 +0200

    Fix wrong unit for timedelta in create_refresh_token function (minutes instead of days)

--- fastapi_test/fastapi_test/security/auth_utils.py
+++ fastapi_test/fastapi_test/security/auth_utils.py
@@ -15,7 +15,7 @@ def create_access_token(data: dict):
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=REFRESH_TOKEN_EXPIRE_DAYS)
+    expire = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire, "type": "refresh"})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
